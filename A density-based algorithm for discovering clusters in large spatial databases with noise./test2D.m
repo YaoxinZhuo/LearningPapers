65b01e7ad1de56edc7dfca0a4d08eaf5f31a4ccb
YaoxinZhuo <zhuoyaoxin@gmail.com>
2016-11-21 19:08:38 +0800

    Thanks to Yuan Meng's data.

--- A density-based algorithm for discovering clusters in large spatial databases with noise./test2D.m
+++ A density-based algorithm for discovering clusters in large spatial databases with noise./test2D.m
@@ -1,5 +1,14 @@
-x=[1000.*rand(10000,2)];
+%随机10000个点范围10k*10k测试
+% x=[10000.*rand(10000,2)];
+% [class,type]=dbscan(x,4,[]);
+% class = class + ones(size(class));%class分类+1，噪声为0类，其他依次1，2，3
+% x=[x,class'];
+% scatter(x(:,1),x(:,2),[],class);
+
+%同学的爬虫奶粉店三项评分数据
+x = xlsread('store_xls.xls');
 [class,type]=dbscan(x,4,[]);
 class = class + ones(size(class));%class分类+1，噪声为0类，其他依次1，2，3
 x=[x,class'];
-scatter(x(:,1),x(:,2),[],class);
+SIZE=1*size(5603,1);
+scatter3(x(:,1),x(:,2),x(:,3),S,class);
